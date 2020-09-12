f6dd73587c9f98963bebe3ac823c2c1145a904bd
momolakeit <56810449+momolakeit@users.noreply.github.com>
2020-09-11 22:55:33 -0400

    add payment service

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,9 @@ import { Router } from '@angular/router';
 export class PaymentService {
 
   constructor(private http:HttpClient,private router:Router) { }
+  getToken(email:string,password:string):Observable<String>{
+    return this.http.post<JwtResponse>(`${environment.authApiUrl}/signin`,{username: email,password:password});
+  }
 
   
 }
