80b1dff051732c224e46457418ae7a8b94170cab
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2020-09-12 23:45:50 -0400

    doing bill service

--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import {BillDTO} from '../models/bill-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BillService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  makeOrder(billDTO:BillDTO,restaurentTableId:number):Observable<BillDTO>{
+    return this.http.post<BillDTO>(`${environment.billUrl}/order/makeOrder`,{billDTO:JSON.stringify(billDTO),restaurentTableId});
+  }
 }
--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
 }
