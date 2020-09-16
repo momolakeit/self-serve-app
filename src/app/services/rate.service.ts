6c12b8967a47e9bf236788b4fc56123ea84c6c5c
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2020-09-15 23:15:26 -0400

    added rate service

--- src/app/services/rate.service.ts
+++ src/app/services/rate.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import {ProductDTO} from '../models/product-dto';
+import {RateDTO} from '../models/rate-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RateService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  find(rate:RateDTO,product:ProductDTO):Observable<RateDTO>{
+      return this.http.post<RateDTO>(`${environment.rateUrl}/createRate`,{rate: rate,product:product});
+  }
 }
