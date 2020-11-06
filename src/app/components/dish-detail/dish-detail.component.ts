95bf709cf281c768ca8202c1967062c1fab6f75e
momolakeit <56810449+momolakeit@users.noreply.github.com>
2020-11-06 07:23:55 -0500

    fix product image bugg

--- src/app/components/dish-detail/dish-detail.component.ts
+++ src/app/components/dish-detail/dish-detail.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ProductDTO } from '../../models/product-dto' ;
-import { CurrentBill} from '../../global/current-bill';
-import { BillService} from '../../services/bill.service';
-import {CheckItemDTO} from '../../models/check-item-dto'
-import {OptionDTO} from '../../models/option-dto'
+import { ProductDTO } from '../../models/product-dto';
+import { CurrentBill } from '../../global/current-bill';
+import { BillService } from '../../services/bill.service';
+import { CheckItemDTO } from '../../models/check-item-dto'
+import { OptionDTO } from '../../models/option-dto'
 import { environment } from '../../../environments/environment';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dish-detail',
@@ -14,23 +14,27 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 })
 export class DishDetailComponent implements OnInit {
 
-  @Input() productDTO :ProductDTO
-  
-  constructor(private currentBill:CurrentBill,private billService :BillService,private snackBar :MatSnackBar) { }
-  imgUrl :string
-  commentaire:string =""
+  @Input() productDTO: ProductDTO
+
+  constructor(private currentBill: CurrentBill, private billService: BillService, private snackBar: MatSnackBar) { }
+  imgUrl: string
+  commentaire: string = ""
 
   ngOnInit(): void {
+    this.setImg();
+  }
+  ngOnChanges(): void {
+    this.setImg();
+  }
+  setImg() {
     console.log(this.productDTO)
     this.imgUrl = environment.baseImgPath + this.productDTO.imgFileDTO.id;
   }
-
-  
-  updateCurrentBill = function (product :ProductDTO): void {
-   this.billService.makeOrder(product,this.commentaire).subscribe(data =>{
-    localStorage.setItem("ongoingBill",JSON.stringify(data)) 
-    this.openSnackBar();
-  });
+  updateCurrentBill = function (product: ProductDTO): void {
+    this.billService.makeOrder(product, this.commentaire).subscribe(data => {
+      localStorage.setItem("ongoingBill", JSON.stringify(data))
+      this.openSnackBar();
+    });
   };
   openSnackBar() {
     this.snackBar.open("Item ordered", "Close", {
@@ -38,16 +42,16 @@ export class DishDetailComponent implements OnInit {
     });
   }
 
-  updateCheckItem = function (checkItemDTO:CheckItemDTO,optionDTO:OptionDTO): void {
-    var currentOption =this.productDTO.options.find(x =>x.id==optionDTO.id);
+  updateCheckItem = function (checkItemDTO: CheckItemDTO, optionDTO: OptionDTO): void {
+    var currentOption = this.productDTO.options.find(x => x.id == optionDTO.id);
     console.log(currentOption.checkItemList);
     currentOption.checkItemList.forEach(element => {
-        element.isActive=false;
+      element.isActive = false;
     });
-    var currentCheckItem =currentOption.checkItemList.find(x=> x.id ==checkItemDTO.id);
-    currentCheckItem.isActive=true;
+    var currentCheckItem = currentOption.checkItemList.find(x => x.id == checkItemDTO.id);
+    currentCheckItem.isActive = true;
     console.log(this.productDTO);
   };
-   
+
 
 }
