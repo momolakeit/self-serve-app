b523e73d3e00c8a6e09342fc5e624fe3a1d9c0f9
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2020-12-13 20:37:40 -0500

    modif env dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -8,30 +8,30 @@ const rolePrefix = 'ROLE_';
 
 export const environment = {
   production: false,
-  authApiUrl: `https://${host}${port}/auth`,
-  contactFormUrl: `https://${host}${port}/support`,
-  employeUrl:`https://${host}${port}/rest/employe`,
-  csrfUrl:`https://${host}${port}/csrf`,
-  productUrl:`https://${host}${port}/product`,
-  menuUrl:`https://${host}${port}/menu`,
-  billUrl:`https://${host}${port}/order`,
-  rateUrl:`https://${host}${port}/rate`,
-  adminUrl:`https://${host}${port}/admin/getOwners`,
-  subscriptionProductUrl:`https://${host}${port}/stripe/fetchSubscriptionProducts`,
-  fetchSubscription:`https://${host}${port}/stripe/retreiveSubscription`,
-  cancelSubscriptionUrl:`https://${host}${port}/stripe/cancelSubscription`,
-  registerOwnerWithStripeUrl :`https://${host}${port}/stripe/createStripeAcccount`,
-  saveStripeAccountId :`https://${host}${port}/stripe/saveAccountId`,
-  fetchStripeAccountId:`https://${host}${port}/stripe/getAccountId`,
-  paymentIntentUrl:`https://${host}${port}/stripe/fetchPaymentIntent`,
-  retrySubscriptionUrl:`https://${host}${port}/stripe/retrySubscription`,
-  createSubscriptionUrl:`https://${host}${port}/stripe/createSubscription`,
-  fetchSubscriptionSessionUrl: `https://${host}${port}/stripe/fetchSubscriptionSession`,
-  paymentRequestIntentUrl:`https://${host}${port}/stripe/fetchPaymentRquestPaymentIntent`,
-  domainFiLE:`https://${host}${port}/order/fetchDomainFile`,
-  baseImgPath:`https://${host}${port}/product/getProductImg/`,
-  kitchenUrl:`https://${host}${port}/rest/kitchen`,
-  qrCodeUrl:`https://${host}${port}/qrcode`,
+  authApiUrl: `http://${host}${port}/auth`,
+  contactFormUrl: `http://${host}${port}/support`,
+  employeUrl:`http://${host}${port}/rest/employe`,
+  csrfUrl:`http://${host}${port}/csrf`,
+  productUrl:`http://${host}${port}/product`,
+  menuUrl:`http://${host}${port}/menu`,
+  billUrl:`http://${host}${port}/order`,
+  rateUrl:`http://${host}${port}/rate`,
+  adminUrl:`http://${host}${port}/admin/getOwners`,
+  subscriptionProductUrl:`http://${host}${port}/stripe/fetchSubscriptionProducts`,
+  fetchSubscription:`http://${host}${port}/stripe/retreiveSubscription`,
+  cancelSubscriptionUrl:`http://${host}${port}/stripe/cancelSubscription`,
+  registerOwnerWithStripeUrl :`http://${host}${port}/stripe/createStripeAcccount`,
+  saveStripeAccountId :`http://${host}${port}/stripe/saveAccountId`,
+  fetchStripeAccountId:`http://${host}${port}/stripe/getAccountId`,
+  paymentIntentUrl:`http://${host}${port}/stripe/fetchPaymentIntent`,
+  retrySubscriptionUrl:`http://${host}${port}/stripe/retrySubscription`,
+  createSubscriptionUrl:`http://${host}${port}/stripe/createSubscription`,
+  fetchSubscriptionSessionUrl: `http://${host}${port}/stripe/fetchSubscriptionSession`,
+  paymentRequestIntentUrl:`http://${host}${port}/stripe/fetchPaymentRquestPaymentIntent`,
+  domainFiLE:`http://${host}${port}/order/fetchDomainFile`,
+  baseImgPath:`http://${host}${port}/product/getProductImg/`,
+  kitchenUrl:`http://${host}${port}/rest/kitchen`,
+  qrCodeUrl:`http://${host}${port}/qrcode`,
 };
 
 export const roles={
@@ -41,6 +41,9 @@ export const roles={
   client: `${rolePrefix}CLIENT`,
   guest: `${rolePrefix}GUEST`,
   admin: `${rolePrefix}ADMIN`
+}
+export const stripeKey ={
+  value : 'pk_test_51HLwKgC5UoZOX4GRWegBa5FvbtsNbi5Cd7Z5WKYB73jelPNuhpzS69dXKe2V3OWTP4XHt5wjGGD3dzEdJw25duSn00Dlctj1NV'
 }
 
 /*
